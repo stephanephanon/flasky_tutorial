8ac4f5237c90dbde270bdab4a97b62f609047058
Stephanie Sydorko <stephanephanon@gmail.com>
2018-09-15 14:01:47 -0400

    add flask_bootstrap, update templates to use base template

--- hello.py
+++ hello.py
@@ -1,12 +1,21 @@
 import logging
 from flask import Flask, render_template
-
+from flask_bootstrap import Bootstrap
 
 logger = logging.getLogger(__name__)
 
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 @app.route('/')
 def index():
